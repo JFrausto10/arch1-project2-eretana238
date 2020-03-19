04911090e13122c59b3086a60a32acbb6d102e85
eretana238 <eretana238@gmail.com>
2020-03-19 13:34:43 -0600

    implemented soundEngine

---
+++ project/src/soundEngine.c
@@ -0,0 +1,12 @@
+#include "soundEngine.h"
+#include "stateMachines.h"
+
+char c4,cs4,d4,ds4,e4,f4,fs4,g4,gs4,a4,as4,b4,c5,cs5,d5,ds5,e5,f5,fs5,g5,gs5,a5,as5,b5;
+
+void play_eighth_note() {
+  
+}
+
+void play_quarter_note() {
+
+}
